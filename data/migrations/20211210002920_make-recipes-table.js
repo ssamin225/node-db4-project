5efd162411f3488099791a56ae4fe07082d33d91
ssamin225 <sadmansamin225@gmail.com>
2021-12-09 21:06:47 -0500

    created tables

--- data/migrations/20211210002920_make-recipes-table.js
+++ data/migrations/20211210002920_make-recipes-table.js
@@ -3,12 +3,12 @@ exports.up = async function(knex) {
   await knex.schema
     .createTable('recipes', tbl => {
       tbl.increments('recipe_id')
-      tbl.string('recipe_name', 256).notNullable().unique()
+      tbl.string('recipe_name', 128).notNullable().unique()
     })
     .createTable('steps', tbl => {
       tbl.increments('step_id')
       tbl.integer('step_number').unsigned().notNullable()
-      tbl.string('step_instructions').notNullable()
+      tbl.string('step_instructions', 256).notNullable()
       tbl.integer('recipe_id')
         .unsigned()
         .notNullable()
@@ -18,15 +18,29 @@ exports.up = async function(knex) {
     })
     .createTable('ingredients', tbl => {
       tbl.increments('ingredient_id')
+      tbl.string('ingredient_name', 128).unique().notNullable()
     })
-    .createTable('quantities', tbl => {
-      tbl.increments('quantity_id')
+    .createTable('step_ingredients', tbl => {
+      tbl.increments('step_ingredients_id')
+      tbl.decimal('quantity').notNullable()
+      tbl.integer('step_id')
+        .unsigned()
+        .notNullable()
+        .references('step_id')
+        .inTable('steps')
+        .onDelete('RESTRICT')
+      tbl.integer('ingredient_id')
+        .unsigned()
+        .notNullable()
+        .references('ingredient_id')
+        .inTable('ingredients')
+        .onDelete('RESTRICT')
     })
   };
 
 exports.down = async function(knex) {
   await knex.schema
-    .dropTableIfExists('quantities')
+    .dropTableIfExists('step_ingredients')
     .dropTableIfExists('ingredients')
     .dropTableIfExists('steps')
     .dropTableIfExists('recipes')
